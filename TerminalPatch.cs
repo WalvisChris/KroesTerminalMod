e59a5875159c7a1a1edad949bdf362374e446916
BuildTools <unconfigured@null.spigotmc.org>
2025-12-21 14:33:34 +0100

    pre

--- TerminalPatch.cs
+++ TerminalPatch.cs
@@ -19,26 +19,46 @@ namespace KroesTerminal.Patches
         {
             string text = __instance.screenText.text.Substring(__instance.screenText.text.Length - __instance.textAdded);
             string command = text.ToLower();
-
-            if (command == "kscan")
-            {
-                KroesTerminal.Log.LogInfo("command: kscan");
-                
-                // LoadNewNode
-
-
-                return false;
-            }
-
-            KroesTerminal.Log.LogInfo("regular command");
+            KroesTerminal.Log.LogInfo($"OnSubmitPrefix: {command}");
             return true;
         }
 
-        [HarmonyPatch("TextPostProcess")]
-        [HarmonyPostfix]
-        private static void TextProcessPostfix(ref string modifiedDisplayText, TerminalNode node)
+        [HarmonyPatch("LoadNewNode")]
+        [HarmonyPrefix]
+        private static bool LoadNewNodePrefix(ref TerminalNode node)
         {
-            KroesTerminal.Log.LogInfo("TextProcessPostfix: " + modifiedDisplayText);
+            KroesTerminal.Log.LogInfo($"(string) displayText: {node.displayText}");
+            KroesTerminal.Log.LogInfo($"(string) terminalEvent: {node.terminalEvent}");
+            KroesTerminal.Log.LogInfo($"(bool) clearPreviousText: {node.clearPreviousText}");
+            KroesTerminal.Log.LogInfo($"(int) maxCharactersToType: {node.maxCharactersToType}");
+
+            KroesTerminal.Log.LogInfo($"(int) buyItemIndex: {node.buyItemIndex}");
+            KroesTerminal.Log.LogInfo($"(int) buyVehicleIndex: {node.buyVehicleIndex}");
+            KroesTerminal.Log.LogInfo($"(bool) isConfirmationNode: {node.isConfirmationNode}");
+            KroesTerminal.Log.LogInfo($"(int) buyRerouteToMoon: {node.buyRerouteToMoon}");
+            KroesTerminal.Log.LogInfo($"(int) displayPlanetInfo: {node.displayPlanetInfo}");
+
+            KroesTerminal.Log.LogInfo($"(int) shipUnlockableID: {node.shipUnlockableID}");
+            KroesTerminal.Log.LogInfo($"(bool) buyUnlockable: {node.buyUnlockable}");
+            KroesTerminal.Log.LogInfo($"(bool) returnFromStorage: {node.returnFromStorage}");
+            KroesTerminal.Log.LogInfo($"(int) itemCost: {node.itemCost}");
+
+            KroesTerminal.Log.LogInfo($"(int) creatureFileID: {node.creatureFileID}");
+            KroesTerminal.Log.LogInfo($"(string) creatureName: {node.creatureName}");
+            KroesTerminal.Log.LogInfo($"(int) storyLogFileID: {node.storyLogFileID}");
+
+            KroesTerminal.Log.LogInfo($"(bool) overrideOptions: {node.overrideOptions}");
+            KroesTerminal.Log.LogInfo($"(bool) acceptAnything: {node.acceptAnything}");
+
+            KroesTerminal.Log.LogInfo($"(CompatibleNoun[]) terminalOptions: {(node.terminalOptions == null ? "null" : node.terminalOptions.Length.ToString())}");
+
+            KroesTerminal.Log.LogInfo($"(int) playSyncedClip: {node.playSyncedClip}");
+
+            KroesTerminal.Log.LogInfo($"(bool) loadImageSlowly: {node.loadImageSlowly}");
+            KroesTerminal.Log.LogInfo($"(bool) persistentImage: {node.persistentImage}");
+
+            return true;
         }
+
     }
 }
